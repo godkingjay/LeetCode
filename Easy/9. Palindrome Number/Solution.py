dd38093882895fc77c76f61ea8d06d7b66156de1
Jarrian <89616705+godkingjay@users.noreply.github.com>
2022-09-02 09:03:12 +0800

    Update Solution.py

--- Easy/9. Palindrome Number/Solution.py
+++ Easy/9. Palindrome Number/Solution.py
@@ -1,10 +1,9 @@
 class Solution(object):
     def isPalindrome(self, x):
-        if x < 0:
+        if x < 0 or (x != 0 and x % 10 == 0):
             return False
-        n = x
         rev = 0
-        while x > 0:
-            rev = rev * 10 + x%10
-            x = x//10
-        return rev == n
+        while x > rev:
+            rev = rev * 10 + x % 10
+            x /= 10
+        return x == rev or x == rev/10
