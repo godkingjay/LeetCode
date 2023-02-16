7dd076d35f3dcbcf07580f4f4246ebe579f8616f
Jarrian <glorianagojar@gmail.com>
2023-02-16 08:48:42 +0800

    add: 104. Maximum Depth of Binary Tree

--- Easy/104. Maximum Depth of Binary Tree/Solution.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int maxDepth(TreeNode root) {
-        if(root == null) return 0;
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return Math.max(left, right) + 1;
-    }
-}
