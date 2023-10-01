e651b12e2fcd7a2a0b49fd46176fec08b8aff9d4
fardeen khan <143899964+fardeen2k@users.noreply.github.com>
2023-10-01 19:29:49 +0530

    Update Solution.java

--- Easy/344. Reverse String/Solution.java
+++ Easy/344. Reverse String/Solution.java
@@ -1,11 +1,47 @@
 class Solution {
-    public void reverseString(char[] s) {
-        char t;
-        int n = s.length;
-        for(int i = 0; i < n/2; i++) {
-            t = s[i];
-            s[i] = s[n - i - 1];
-            s[n - i - 1] = t;
-        }
+  public String reverseWords(String s) {
+    StringBuilder sb = new StringBuilder(s).reverse(); // Reverse the whole string
+    reverseWords(sb, sb.length());                     // Reverse each word
+    return cleanSpaces(sb, sb.length());               // Clean up spaces
+  }
+
+  private void reverseWords(StringBuilder sb, int n) {
+    int i = 0;
+    int j = 0;
+
+    while (i < n) {
+      while (i < j || i < n && sb.charAt(i) == ' ') // Skip spaces
+        ++i;
+      while (j < i || j < n && sb.charAt(j) != ' ') // Skip non spaces
+        ++j;
+      reverse(sb, i, j - 1); // Reverse the word
     }
+  }
+
+  // Trim leading, trailing, and middle spaces
+  private String cleanSpaces(StringBuilder sb, int n) {
+    int i = 0;
+    int j = 0;
+
+    while (j < n) {
+      while (j < n && sb.charAt(j) == ' ') // Skip spaces
+        ++j;
+      while (j < n && sb.charAt(j) != ' ') // Keep non spaces
+        sb.setCharAt(i++, sb.charAt(j++));
+      while (j < n && sb.charAt(j) == ' ') // Skip spaces
+        ++j;
+      if (j < n) // Keep only one space
+        sb.setCharAt(i++, ' ');
+    }
+
+    return sb.substring(0, i).toString();
+  }
+
+  private void reverse(StringBuilder sb, int l, int r) {
+    while (l < r) {
+      final char temp = sb.charAt(l);
+      sb.setCharAt(l++, sb.charAt(r));
+      sb.setCharAt(r--, temp);
+    }
+  }
 }
