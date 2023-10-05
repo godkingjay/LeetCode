c52d5d233a483b256c4abbc6ee5970805721b525
RUCHIKA YADAV <86114973+ruchikayadav1408@users.noreply.github.com>
2023-10-05 08:58:50 +0530

    Create solution.java

---
+++ Easy/14. Longest Common Prefix/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+          Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        int idx = 0;
+        while(idx < s1.length() && idx < s2.length()){
+            if(s1.charAt(idx) == s2.charAt(idx)){
+                idx++;
+            } else {
+                break;
+            }
+        }
+        return s1.substring(0, idx);
+    }
+}
