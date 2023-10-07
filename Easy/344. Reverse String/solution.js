ff3bbb5f7022d2eb5ae0ff3e4f27618d3b008479
Md Imran Hossen <54544265+imranhc0@users.noreply.github.com>
2023-10-07 09:42:56 +0600

    solution 2 remains same, add new solution

--- Easy/344. Reverse String/solution.js
+++ Easy/344. Reverse String/solution.js
@@ -2,8 +2,10 @@
 const reverseString = function(s) {
     return s.reverse()
 };
-
-//solution 2
+ 
+// solution 2 
+// this solution got error about memory excceded, but this is a another way to do
+// This solution not accepted in leetcode but it's work fine
 const reverseString = function(s) {
     let res = []
     for(let i  = s.length -1; i >= 0 ; i--) {
@@ -11,4 +13,22 @@ const reverseString = function(s) {
     }
 
     return res
+};
+
+//solution 3
+//this is 2 pointer solution
+const reverseString = function(s) {
+    let left = 0;
+    let right = s.length - 1 ;
+
+    while(left < right){
+        const temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        left += 1;
+        right -= 1;
+    }
+
+    return s;
 };
