230979538fee5e773900367a5972bf04e0113e27
Aman <amanbisht@gmail.com>
2023-10-19 18:47:01 +0530

    Added Python Code For 328th leetcode problem

--- Medium/328. Odd Even Linked List/sol.py
+++ Medium/328. Odd Even Linked List/sol.py
@@ -1,21 +1,18 @@
-'''    
-    # LeetCode 328.
-    # Problem Statement:
-    # Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
-    # The first node is considered odd, and the second node is even, and so on.
-    # Note that the relative order inside both the even and odd groups should remain as it was in the input.
+# LeetCode 328.
+# Problem Statement:
+# Given the head of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return the reordered list.
+# The first node is considered odd, and the second node is even, and so on.
+# Note that the relative order inside both the even and odd groups should remain as it was in the input.
 
-    # Test Cases:
+# Test Cases:
 
-    # Example 1:
-    # Input: head = [1,2,3,4,5]
-    # Output: [1,3,5,2,4]
+# Example 1:
+# Input: head = [1,2,3,4,5]
+# Output: [1,3,5,2,4]
 
-    # Example 2:
-    # Input: head = [2,1,3,5,6,4,7]
-    # Output: [2,3,6,7,1,5,4]
-
-'''
+# Example 2:
+# Input: head = [2,1,3,5,6,4,7]
+# Output: [2,3,6,7,1,5,4]
 
 # Solution:
 # Definition for singly-linked list.
@@ -26,6 +23,7 @@
 
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # Initialize two dummy nodes to represent even and odd nodes.
         dummy_even = ListNode(0)
         dummy_odd  = ListNode(0)
 
@@ -34,18 +32,22 @@ class Solution:
 
         i = 1
 
-        while(head):
+        while head:
             if i % 2 == 0:
+                # Even node, connect it to the even list.
                 even.next = head
                 even = even.next
             else:
+                # Odd node, connect it to the odd list.
                 odd.next = head
                 odd = odd.next
 
             i += 1
             head = head.next
 
+        # Connect the odd list to the even list to form the final result.
         odd.next = dummy_even.next
         even.next = None
 
+        # Return the merged list of odd and even nodes.
         return dummy_odd.next
