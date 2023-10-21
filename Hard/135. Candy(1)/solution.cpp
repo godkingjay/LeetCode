879613a029cd70f2af9960ce7faf8f0d8829dd9b
pv-vardhan <vishnuvardhan0811@gmail.com>
2023-10-21 21:23:56 +0530

    135. Candy problem java and c++ solution

---
+++ Hard/135. Candy(1)/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int candy(std::vector<int>& ratings) {
+        int n = ratings.size();
+        std::vector<int> candies(n, 1);
+
+        for (int i = 1; i < n; ++i) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; --i) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = std::max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int totalCandies = 0;
+        for (int candy : candies) {
+            totalCandies += candy;
+        }
+
+        return totalCandies;
+    }
+};
