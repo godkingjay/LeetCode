01554b564ed9dce15497593128087c692b86a1b5
Jarrian Gojar <glorianagojar@gmail.com>
2023-10-22 08:41:16 +0800

    Update Solution.java

--- Medium/55. Jump Game/Solution.java
+++ Medium/55. Jump Game/Solution.java
@@ -1,5 +1,5 @@
 public class Solution {
-    public boolean jumpPossible(int[] nums) {
+    public boolean canJump(int[] nums) {
         int maxReach = 0;
 
         for (int i = 0; i < nums.length; i++) {
@@ -15,17 +15,5 @@ public class Solution {
         }
 
         return false;
-    }
-
-    public static void main(String[] args) {
-        Solution jumpGame = new Solution();
-        int[] nums1 = {3, 2, 1, 0, 4};
-        int[] nums2 = {2, 3, 1, 1, 4};
-
-        boolean result1 = jumpGame.jumpPossible(nums1);
-        boolean result2 = jumpGame.jumpPossible(nums2);
-
-        System.out.println("Can reach the last index (nums1): " + result1); // Output: false
-        System.out.println("Can reach the last index (nums2): " + result2); // Output: true
     }
 }
