bf36a5f244f8fdf72c1e0ced937e3015706351e1
omarihab99 <omar.ihab.mohamed99@gmail.com>
2023-10-26 14:26:58 +0300

    Added new problem "Permutations II" with java solution

---
+++ Medium/47. Permutations II/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    List<List<Integer>> res;
+    Set<List<Integer>> set = new HashSet();
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        boolean[] visited = new boolean[nums.length];
+        backtrack(new ArrayList(), visited, nums);
+        res = new ArrayList(set);
+        return res;
+    }
+    private void backtrack(List<Integer> sub,boolean[] visited,int[] nums){
+        if(sub.size() == nums.length){
+            set.add(new ArrayList(sub));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(!visited[i]){
+                visited[i] = true;
+                sub.add(nums[i]);
+                backtrack(sub, visited,nums);
+                sub.remove(sub.size()-1);
+                visited[i] = false;
+            }
+
+        }
+    }
+}
