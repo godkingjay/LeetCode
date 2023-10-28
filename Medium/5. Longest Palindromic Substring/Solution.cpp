da2c77352733a5457ca3abea9ffd6fa0cfed8d51
krishprav <118839797+krishprav@users.noreply.github.com>
2023-10-28 12:24:57 +0530

    Update Solution.cpp

--- Medium/5. Longest Palindromic Substring/Solution.cpp
+++ Medium/5. Longest Palindromic Substring/Solution.cpp
@@ -1,35 +1,23 @@
-#include <string>
-
 class Solution {
 public:
-    std::string longestPalindrome(std::string s) {
-        std::string res = "";
-        int reslen = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            // odd length
-            int left = i, right = i;
-            while (left >= 0 && right < s.length() && s[left] == s[right]) {
-                if (right - left + 1 > reslen) {
-                    res = s.substr(left, right - left + 1);
-                    reslen = right - left + 1;
-                }
-                left -= 1;
-                right += 1;
-            }
-
-            // even length;
-            left = i;
-            right = i + 1;
-            while (left >= 0 && right < s.length() && s[left] == s[right]) {
-                if (right - left + 1 > reslen) {
-                    res = s.substr(left, right - left + 1);
-                    reslen = right - left + 1;
-                }
-                left -= 1;
-                right += 1;
-            }
+    string ans = "";
+    void expand(string &s , int left ,int right)
+    {
+        while(left >= 0 &&  right < s.size())
+        {
+            if(s[left] != s[right])
+                break;
+            left--,right++;
         }
-        return res;
+        if(ans.size() < right - left )
+            ans = s.substr(left + 1 , right - left - 1);
+    }
+    string longestPalindrome(string s) {
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            expand(s , i , i);
+            expand(s , i , i+1);
+        }
+        return ans;
     }
 };
