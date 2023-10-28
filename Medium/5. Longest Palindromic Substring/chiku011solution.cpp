5036ef64e8303111fd8e7ebf199ba78110af0d3e
chiku <chintukumar0460@gmail.com>
2023-10-28 12:36:52 +0530

    add leetcode tested solution

--- Medium/5. Longest Palindromic Substring/chiku011solution.cpp
+++ Medium/5. Longest Palindromic Substring/chiku011solution.cpp
@@ -1,54 +1,34 @@
-// A C++ solution for longest palindrome
 
-#include <bits/stdc++.h>
-using namespace std;
 
-// Function to print a substring str[low..high]
-void printSubStr(string str, int low, int high)
-{
-	for (int i = low; i <= high; ++i)
-		cout << str[i];
-}
-
-// This function prints the longest palindrome substring
-// It also returns the length of the longest palindrome
-int longestPalSubstr(string str)
-{
-	// Get length of input string
-	int n = str.size();
-
-	// All substrings of length 1 are palindromes
-	int maxLength = 1, start = 0;
-
-	// Nested loop to mark start and end index
-	for (int i = 0; i < str.length(); i++) {
-		for (int j = i; j < str.length(); j++) {
-			int flag = 1;
-
-			// Check palindrome
-			for (int k = 0; k < (j - i + 1) / 2; k++)
-				if (str[i + k] != str[j - k])
-					flag = 0;
-
-			// Palindrome
-			if (flag && (j - i + 1) > maxLength) {
-				start = i;
-				maxLength = j - i + 1;
-			}
-		}
-	}
-
-	cout << "Longest palindrome substring is: ";
-	printSubStr(str, start, start + maxLength - 1);
-
-	// Return length of LPS
-	return maxLength;
-}
-
-// Driver Code
-int main()
-{
-	string str = "forgeeksskeegfor";
-	cout << "\nLength is: " << longestPalSubstr(str);
-	return 0;
-}
+class Solution {
+public:
+    bool check (int i,int j , string s){
+        while(i<=j){
+            if(s[i] != s[j]){
+                return false;
+            }else{
+                i++;
+                j--;
+            }
+        }
+            return true;
+    }
+    string longestPalindrome(string s) {
+        int maxlen=0;
+        int index =0;
+        for(int i =0;i<s.size();i++){
+            for(int j =i;j<s.size();j++){
+                if(s[i]==s[j]){
+                    bool ans = check(i,j,s);
+                    if(ans){
+                        if(j-i+1 > maxlen){
+                            maxlen = j-i+1;
+                            index =i;
+                        }
+                    }
+                }
+            }
+        }
+        return s.substr(index,maxlen);
+    }
+};
