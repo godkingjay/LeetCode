424b62ac0731ec1b85aa91648c6a3b1b163590e0
Jarrian Gojar <glorianagojar@gmail.com>
2023-10-31 08:56:04 +0800

    Update Length_of_last_word.js

--- Easy/58. Length of Last Word/Length_of_last_word.js
+++ Easy/58. Length of Last Word/Length_of_last_word.js
@@ -3,17 +3,8 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    s = s.trim();
-    
-    
-    const words = s.split(" ");
-    return words[words.length - 1].length;
-  };
-  
-  const input1 = "Hello World";
-  const input2 = "Welcome to Hacktoberfest ";
-  const input3 = "luffy is still joyboy";
-  
-  console.log(lengthOfLastWord(input1)); 
-  console.log(lengthOfLastWord(input2)); 
-  console.log(lengthOfLastWord(input3)); 
+  s = s.trim();
+
+  const words = s.split(" ");
+  return words[words.length - 1].length;
+};
